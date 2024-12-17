22dfb127a7318dbabc6d3bf54dfff25201eabf70
Steak Barbare <cortodufour@gmail.com>
2024-12-17 06:55:14 +0100

    Fix: Pokemon var to Shit

--- routes/getAllShit.js
+++ routes/getAllShit.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const pokemon = require("../services/getAllShit.js");
+const shit = require("../services/getAllShit.js");
 
 router.get("/", async function (req, res, next) {
   try {
-    res.json(await pokemon.getMultiple(req.query.page));
+    res.json(await shit.getMultiple(req.query.page));
   } catch (err) {
     console.error(`Error while getting Shit data`, err.message);
     next(err);
--- routes/getShitById.js
+++ routes/getShitById.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const pokemon = require("../services/getShitById.js");
+const shit = require("../services/getShitById.js");
 
 router.get("/:id", async function (req, res, next) {
   try {
-    res.json(await pokemon.getMultiple(req.query.page, req.params.id));
+    res.json(await shit.getMultiple(req.query.page, req.params.id));
   } catch (err) {
     console.error(`Error while getting Shit data`, err.message);
     next(err);
